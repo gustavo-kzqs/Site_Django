0716fd07d89686e78cf8b3709420600021cc1eec
gustavo-kzqs <gustavo.kazeker@gmail.com>
2025-09-15 15:48:36 -0300

    Adição de author e slug na aba de criação de posts.

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.text import slugify
+
 
 class Post(models.Model):
+    STATUS_CHOICES= (
+        (0, 'Draft'),
+        (1, 'Published')
+    )
     title = models.CharField(max_length=200)   
     content = models.TextField()  
+    slug = models.SlugField(max_length=200, unique=True, blank=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
     created_at = models.DateTimeField(auto_now_add=True) 
-    updated_at = models.DateTimeField(auto_now=True)     
+    updated_at = models.DateTimeField(auto_now=True) 
+    status =  models.IntegerField(choices=STATUS_CHOICES, default=0)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)    
+
 
     def __str__(self):
         return self.title
